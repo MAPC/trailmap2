c127bf5b360c60da140d1b6b13ec30190b68c33a
Matt Gardner <wilburnforce@gmail.com>
2016-12-07 22:27:26 -0500

    initial gh-pages commit

--- app/components/leaflet-map.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import EmberLeafletMap from 'ember-leaflet/components/leaflet-map';
-
-export default EmberLeafletMap.extend({
-
-});
--- app/components/ui-checkbox.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import Checkbox from 'semantic-ui-ember/components/ui-checkbox';
-
-
-export default Checkbox.extend({
-});
