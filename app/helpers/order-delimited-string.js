59e62654974a8f51ff5f3abc359ba8a6b1bf43b5
Matt Gardner <wilburnforce@gmail.com>
2017-01-05 13:34:46 -0500

    Fixes sublayer checkbox issue
    Closes #27

---
+++ app/helpers/order-delimited-string.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function orderDelimitedString(context, options) {
+    if(context){
+      let tempArr = context[0].trim().split(options["delimiter"]).sortBy('').toString();
+
+      return tempArr;
+    }
+  }
+
+export default Ember.Helper.helper(orderDelimitedString);
---
+++ tests/unit/helpers/split-string-test.js
@@ -0,0 +1,12 @@
+
+import { splitString } from 'trailmap/helpers/split-string';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | split string');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = splitString([42]);
+  assert.ok(result);
+});
+
