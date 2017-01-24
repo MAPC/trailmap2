b5a7fceedf6e0c088a4ae09d5bfcc813135bd5fb
Matt Gardner <wilburnforce@gmail.com>
2017-01-24 18:23:47 -0500

    Fixing landing page buttons

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,72 +1,32 @@
 import Ember from 'ember';
 import computed from 'ember-computed';
-import cartodbSql from '../utils/cartodb-sql';
-import config from '../config/environment';
-import request from 'ember-ajax';
-
-let filters = config.APP.filters;
-let paramNames = filters.uniqBy('alias').mapBy('alias').concat(['showProposed']);
 
 export default Ember.Controller.extend({
-  ajax: Ember.inject.service(),
   queryParams: ['protected','shared','bike_lane','walk','multi_use_path','landline'],
-  // these long field sql pieces need to be moved into environment. the API should change to allow for this.
-  walkingtrailsQuery: makeSql('walk_fac_type', ["(CASE WHEN fac_type=1 THEN 1 WHEN fac_type=2 THEN 1 WHEN fac_type=3 THEN 1 END) AS fac_type_simp_code", "CASE WHEN fac_type=1 THEN 'Paved Walkway' WHEN fac_type=2 THEN 'Footpath' WHEN fac_type=3 THEN 'Cartpath' END AS fac_type_name"]),
-  bikefacilitiesQuery: makeSql('bike_fac_type', ["(CASE WHEN fac_type=1 then 'Bike Lane' WHEN fac_type=2 THEN 'Cycle Track' WHEN fac_type=3 THEN 'Sign-posted on-road bike route' WHEN fac_type=4 THEN 'Paved bike shoulder' WHEN fac_type=5 THEN 'Shared-Use Path' WHEN fac_type=7 THEN 'Bicycle / Pedestrian priority roadway' WHEN fac_type=9 THEN 'Marked Shared-Lane' END) AS fac_type_str"]),
-  landlineregionalgreenwaysQuery: makeSql('land_line_type'),
-  dualtrailsQuery: makeSql('dual_fac_type', ["(CASE WHEN fac_type=1 then 'Bike Lane' WHEN fac_type=2 THEN 'Cycle Track' WHEN fac_type=3 THEN 'Sign-posted on-road bike route' WHEN fac_type=4 THEN 'Paved bike shoulder' WHEN fac_type=5 THEN 'Shared-Use Path' WHEN fac_type=7 THEN 'Bicycle / Pedestrian priority roadway' WHEN fac_type=9 THEN 'Marked Shared-Lane' END) AS fac_type_str"]),
-  geolocation: Ember.inject.service(),
 
-  sqlMapping: function() {
-    // order matters.
-    return [this.get('landlineregionalgreenwaysQuery'),
-            this.get('bikefacilitiesQuery'),
-            this.get('walkingtrailsQuery'),
-            this.get('dualtrailsQuery')];
-
-  }.property('walkingtrailsQuery,bikefacilitiesQuery,landlineregionalgreenwaysQuery,dualtrailsQuery'),
-
-  protected: false,
-  shared: false,
-  bike_lane: false,
-  walk: false,
-  multi_use_path: false,
+  protected: true,
+  shared: true,
+  bike_lane: true,
+  walk: true,
+  multi_use_path: true,
   landline: false,
-  proposed: false,
   sublayers: function() {
     // order matters.
     return this.getProperties('protected','shared','bike_lane','walk','multi_use_path','landline');
   }.property('protected,shared,bike_lane,walk,multi_use_path,landline'),
 
-  bike_fac_type: '1,2,9',
-  walk_fac_type: '1',
-  dual_fac_type: '5',
-  land_line_type: false,
+
   basemap: 'default',
-  showProposed: false,
 
   zoom: 11,
   lat: 42.32657,
   lng: -71.352,
 
+  geolocation: Ember.inject.service(),
   currentLocation: null,
   customIcon: Ember.computed(function() {
     return L.icon.pulse({iconSize:[20,20],color:'blue'});
   }),
-
-  bikeMetaData: config.APP.domains.bike_fac_type,
-  bikeFacChecklist: computed('bike_fac_type', 'bikeMetaData', function() {
-    let codedValues = this.get('bikeMetaData.domain.codedValues');
-    return codedValues.map((c) => {
-      return { label: c.name, value: c.code, color: c.color };
-    });
-  }),
-
-  walkMetaData: config.APP.domains.walk_fac_type,
-  dualMetaData: config.APP.domains.dual_fac_type,
-  regionalLandLineMetaData: config.APP.domains.landline_regional_greenways,
-
-
 
   actions: {
     toggleAll(props) {
@@ -102,13 +62,13 @@ export default Ember.Controller.extend({
         this.setProperties({lat: latitude, lng: longitude, zoom: 18 });
       });
       
-      this.transitionToRoute('filters');
+      this.transitionToRoute('filters', { queryParams: { bike_lane: false, 
+                                                          protected: false, 
+                                                          shared: false,
+                                                          walk: true,
+                                                          multi_use_path: false,
+                                                          landline: false } });
+
     }
   }
 });
-
-function makeSql(alias, fields) {
-  return computed(...paramNames, function() {
-    return cartodbSql(this, filters, alias, fields);
-  });
-}
--- app/controllers/filters.js
+++ app/controllers/filters.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  applicationController: Ember.inject.controller('application')
+  applicationController: Ember.inject.controller('application'),
+  actions: {
+    toggleAll(props) {
+      let applicationController = this.get('applicationController');
+      let properties = applicationController.getProperties(props);
+      let values = Object.values(properties);
+      let keys = Object.keys(properties);
+      if(values.includes(true)) {
+        keys.forEach((key) => {
+          applicationController.set(key, false);
+        });
+      } else {
+        keys.forEach((key) => {
+          applicationController.set(key, true);
+        });
+      }
+    }
+  }
 });
